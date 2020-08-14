d7e5f81301471fd3792e1ae49587e96b237f71d9
Back2Hack <69637788+back-2-hack@users.noreply.github.com>
2020-08-14 14:55:11 +0530

    Add files via upload

---
+++ firewall/ufw.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+read -p $'    \033[0;31m\033[4;31m\033[47m!!! MUST ROOT OTHERWISE SCRIPT WILL NOT WORK PROPERLY !!!    \e[1;33m\e[0m\e[1;33m Continue(C) | Go Back(G) : \e[0m' option
+echo""
+echo""
+echo""
+
+if [ "option" == "h" ]; then
+  echo hi
+  
+;;
+
+
+
+
---
+++ start.sh
@@ -0,0 +1,254 @@
+#!/bin/bash
+clear
+echo ""
+echo ""
+echo ""
+
+read -p $'    \033[0;31m\033[4;31m\033[47m!!! MUST ROOT OTHERWISE SCRIPT WILL NOT WORK PROPERLY !!!    \e[1;33m\e[0m\e[1;33m Continue(C) | Go Back(G) : \e[0m' option
+echo""
+echo""
+echo""
+
+if [[ $option == *'G'* ]]; then
+./start.sh
+clear
+exit
+fi
+if [[ $option == *'g'* ]]; then
+./start.sh
+clear
+exit
+fi
+
+clear
+
+clear
+
+printf " \e[0m\n"
+      echo -e $'\e[1;33m\e[0m\e[1;37m                                                               ██                        \e[1;33m\e[0m\e[1;37m ██         \e[0m'
+      echo -e $'\e[1;33m\e[0m\e[1;37m                                                               ██                        \e[1;33m\e[0m\e[1;37m ██         \e[0m'
+      echo -e $'\e[1;33m\e[0m\e[1;37m                                                               ██            \e[0m''\e[1;33m\e[0m\e[1;32m ████████   \e[1;33m\e[0m\e[1;37m ██         \e[0m'
+      echo -e $'\e[1;33m\e[0m\e[1;37m                                                               ██              \e[0m''\e[1;33m\e[0m\e[1;32m      █   \e[1;33m\e[0m\e[1;37m ██         \e[0m'
+      echo -e $'\e[1;33m\e[0m\e[1;37m                                                               ██████████      \e[0m''\e[1;33m\e[0m\e[1;32m      █   \e[1;33m\e[0m\e[1;37m ████████████ \e[0m'
+      echo -e $'\e[1;33m\e[0m\e[1;37m                                                               ██      ██    \e[0m''\e[1;33m\e[0m\e[1;32m ████████   \e[1;33m\e[0m\e[1;37m ██        ██ \e[0m'
+      echo -e $'\e[1;33m\e[0m\e[1;37m                                                               ██      ██    \e[0m''\e[1;33m\e[0m\e[1;32m █          \e[1;33m\e[0m\e[1;37m ██        ██ \e[0m'
+      echo -e $'\e[1;33m\e[0m\e[1;37m                                                               ██      ██    \e[0m''\e[1;33m\e[0m\e[1;32m █          \e[1;33m\e[0m\e[1;37m ██        ██ \e[0m'
+      echo -e $'\e[1;33m\e[0m\e[1;37m                                                               ██████████    \e[0m''\e[1;33m\e[0m\e[1;32m ████████   \e[1;33m\e[0m\e[1;37m ██        ██ \e[0m' 
+
+printf " \e[0m\n"
+echo -e $'\e[1;37m\e[0m\e[1;37m	        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>	\e[0m''\e[1;37m\e[0m\e[1;37m	<< +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[0m'
+	    echo -e "\e[93m			B 2 H | E r r o r | S o l v e r	Version : 1.0"      								"				 Developer - www.github.com/back-2-hack	"
+	    echo -e $'\e[1;37m\e[0m\e[1;37m    	   	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>	\e[0m''\e[1;37m\e[0m\e[1;37m	<< +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[0m'
+    echo ""                                                
+echo -e $'\e[1;32m\e[0m\e[1;32m 									 ACTIVE / RUN ANY OPTION  \e[0m'
+echo -e $'\e[1;37m\e[0m\e[1;37m 								  < < < ------------------------ > > >\e[0m'
+echo ""
+
+printf "									\e[1;32m[\e[0m\e[1;77m01\e[0m\e[1;32m]\e[0m\e[1;93m Run error solver\e[0m	 \e[0m\n"  
+printf " \e[0m\n"              
+printf "							\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Active Security Mode\e[0m   \e[1;92m[\e[0m\e[1;77m03\e[0m\e[1;92m]\e[0m\e[1;93m Diactive Security Mode\e[0m       \e[0m\n"
+printf ""
+printf " \e[0m\n" 
+printf "							\e[1;92m[\e[0m\e[1;77m04\e[0m\e[1;92m]\e[0m\e[1;93m Shutdown\e[0m   	    \e[1;92m[\e[0m\e[1;77m05\e[0m\e[1;92m]\e[0m\e[1;93m Reboot\e[0m       \e[0m\n"
+
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf      "
+			 			        \e[1;92m[\e[0m\e[1;77m99\e[0m\e[1;92m]\e[0m\e[1;93m Exit         \n"                                                                       
+                                                                                                                
+                                                                            
+
+
+echo "" 
+
+                
+                 read -p $'\e[0m\e[1;32mB2H > \e[0m' option
+		 case $option in 
+			 
+                         1)clear
+
+
+
+
+clear
+
+sudo chmod 777 /etc/apt
+sudo chmod +x /etc/apt
+sudo cp files/sources.list /etc/apt/sources.list
+printf 'coping files'
+clear
+sleep 2
+printf 'files copied'
+clear
+sudo apt-get update
+echo 'Your error is gone'
+sleep 2
+sudo ./start.sh
+
+
+
+                ;;
+		
+		
+		
+		01)clear
+
+
+
+
+
+clear
+
+sudo chmod 777 /etc/apt
+sudo chmod +x /etc/apt
+sudo cp files/sources.list /etc/apt/sources.list
+printf 'coping files'
+sleep 1
+printf 'files copied'
+sleep 1
+clear
+sudo apt-get update
+echo 'Your error is gone'
+sleep 2
+sudo ./start.sh
+
+
+
+                ;;
+
+
+
+                ;;
+		
+		
+		
+		
+		2)
+clear
+
+sudo chmod 777 /etc/apt
+sudo chmod +x /etc/apt
+sudo cp files/sources.list /etc/apt/sources.list
+printf 'coping files'
+clear
+sleep 2
+printf 'files copied'
+clear
+sudo apt-get update
+echo 'Your error is gone'
+sleep 2
+sudo ./start.sh
+		clear
+		sudo apt-get install ufw
+		sudo ufw enable
+		clear
+		echo 'Security mode is activated'
+		sleep 3
+		sudo ./start.sh
+		;;
+		02)
+clear
+
+sudo chmod 777 /etc/apt
+sudo chmod +x /etc/apt
+sudo cp files/sources.list /etc/apt/sources.list
+printf 'coping files'
+clear
+sleep 2
+printf 'files copied'
+clear
+sudo apt-get update
+echo 'Your error is gone'
+sleep 2
+sudo ./start.sh
+		clear
+		sudo apt-get install ufw
+		sudo ufw enable
+		clear
+		echo 'Security mode is activated'
+		sleep 3
+		sudo ./start.sh
+	    	;;
+
+
+                3)
+clear
+
+sudo chmod 777 /etc/apt
+sudo chmod +x /etc/apt
+sudo cp files/sources.list /etc/apt/sources.list
+printf 'coping files'
+clear
+sleep 2
+printf 'files copied'
+clear
+sudo apt-get update
+echo 'Your error is gone'
+sleep 2
+sudo ./start.sh
+                clear
+		sudo apt-get install ufw
+		sudo ufw disable
+		clear
+		echo 'Security mode is deactivated'
+		sleep 3
+		sudo ./start.sh
+		;;
+		03)
+clear
+
+sudo chmod 777 /etc/apt
+sudo chmod +x /etc/apt
+sudo cp files/sources.list /etc/apt/sources.list
+printf 'coping files'
+clear
+sleep 2
+printf 'files copied'
+clear
+sudo apt-get update
+echo 'Your error is gone'
+sleep 2
+sudo ./start.sh
+		clear
+		sudo apt-get install ufw
+		sudo ufw disable
+		clear
+		echo 'Security mode is deactivated'
+		sleep 3
+		sudo ./start.sh
+	        ;;
+		
+		4)
+		clear
+		sudo poweroff
+		;;
+		04)
+		clear
+		sudo poweroff
+	        ;;
+	        
+	        5)
+		clear
+		sudo reboot
+		;;
+		05)
+		clear
+		sudo reboot
+	        ;;
+	        
+
+
+                99)
+                clear
+                echo -e $'
+                \e[1;32m[\e[0m\e[1;77m+\e[0m\e[1;32m]\e[0m\e[1;33m<<!! THANKS FOR JOINING B2H ERROR SOLVING !!>> \e[0m'
+                
+                   sleep 2
+                   
+                   exit
+                   
+                ;;
+
+
+                esac
+                
